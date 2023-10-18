d7a687c83b440bfc60f116e4dcbcd4c4817fc24d
Angel Murillo <aangelmurv@gmail.com>
2023-10-18 13:25:51 -0600

    Archivos de readme y modificar

---
+++ modificar.sql
@@ -0,0 +1,16 @@
+#Guía
+# Tablas de SQL
+Aqui estaran alojados toda la base de datos de integradora
+- tablas
+- insercciones
+- Vistas
+- Subconsultas
+- tiggers
+- Y todo lo demás necesario
+
+## Hacer que funcione
+Primero ejecutas el archivo creacion-tablas.sql para que las tablas esten creadas.
+SOLO EL ARCHIVO creacion-tablas.sql ES LA CREACION DE TABLAS
+SI ES NECESARIO MODIFICAR CAMPOS se alojara en modificar.sql
+
+Para las insercciones se ejecutará el archivos insercciones.sql. Tomemos en cuenta que solo habra insercciones para usar de manera de prueba, ya tendiendo interfaces se hará de manera de interfaz las insercciones
