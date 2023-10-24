c869f3d442e987f3c680305549e4a441e898df8a
Angel Murillo <aangelmurv@gmail.com>
2023-10-23 23:13:14 -0600

    Insercciones

--- insercciones.sql
+++ insercciones.sql
@@ -6,10 +6,46 @@ INSERT INTO medidas (nom_medida, uni_medida) VALUES ("Militros", "ml"), ("Pieza"
 
 INSERT INTO tipos_cafe (tipo_cafe) VALUES ("Regular"), ("Descafeinado");
 
-INSERT INTO productos_extra (nombre_pe, precio_unitario_pe) VALUES ("Leche entera", 0.00), ("Leche almendras", 15.00), ("Leche de avena", 10.00);
+INSERT INTO productos_extra (nombre_pe, precio_unitario_pe) 
+VALUES ("Leche entera", 0.00), ("Leche almendras", 15.00), ("Leche de avena", 10.00);
 
-INSERT INTO categorias (nom_categoria, img_categoria) VALUES ("Postres", "postres.jpg"), ("Cafes", "cafes.jpg");
+INSERT INTO categorias (nom_categoria, img_categoria) 
+VALUES ("Postres", "postres.jpg"), ("Cafes", "cafes.jpg");
 
-INSERT INTO usuarios (nombre_usuario, apellido_p_usuario, apellido_m_usuario, email_usuario, contrasena_usuario, foto_perfil_usuario, telefono_usuario, status_usuario, creado_en_usuario, id_rol) VALUES ("Alfredo", "Gomez", "Gonzalez", "imjuantrc@gmail.com", "1234", "pug.jpg", "+528718451815", 1, NOW(), 3), ("Antonio", "Salais", "Escamilla", "gamerantonio222s@gmail.com", "1234", "Default.jpg", "+528717102354", 1, NOW(), 2), ("Jose Angel", "Murillo", "Verastegui", "aangelmurv@gmail.com", "1234", "murillo.jpg", "+528715726548", 1, NOW(), 1), ("Adrian", "Godttfried", "Gutierrez", "adriangottfried@gmail.com", "1234", "Default.jpg", "+528180919984", 1, NOW(), 1), ("Jose Angel", "Garcia", "De La Cruz", "joseangelgarciadelacruz5@gmail.com", "1234", "Default.jpg", "+528713714727", 1, NOW(), 3);
+INSERT INTO usuarios 
+(nombre_usuario, apellido_p_usuario, apellido_m_usuario, email_usuario, contrasena_usuario, 
+foto_perfil_usuario, telefono_usuario, status_usuario, creado_en_usuario, id_rol) 
+VALUES 
+("Alfredo", "Gomez", "Gonzalez", "imjuantrc@gmail.com", "1234", "pug.jpg", "+528718451815", 1, NOW(), 3), 
+("Antonio", "Salais", "Escamilla", "gamerantonio222s@gmail.com", "1234", "Default.jpg", "+528717102354", 1, NOW(), 2), 
+("Jose Angel", "Murillo", "Verastegui", "aangelmurv@gmail.com", "1234", "murillo.jpg", "+528715726548", 1, NOW(), 1), 
+("Adrian", "Godttfried", "Gutierrez", "adriangottfried@gmail.com", "1234", "Default.jpg", "+528180919984", 1, NOW(), 1), 
+("Jose Angel", "Garcia", "De La Cruz", "joseangelgarciadelacruz5@gmail.com", "1234", "Default.jpg", "+528713714727", 1, NOW(), 3);
 
-/* INSERT INTO empleados (curp_empleado, rfc_empleado, salario_mes_empleado, id_usuario) VALUES ("AAAAAAAAAAAAAAAAAA", "AAAAAAAAAAAAA", 850.20, ), ("AAAAAAAAAAAAAAAAAB", "AAAAAAAAAAAAB", 900, )*/ 
+INSERT INTO empleados
+(curp_empleado, rfc_empleado, salario_mes_empleado, id_usuario)
+VALUES
+("AAA", "AAA", 1200.00, 2),
+("BBB", "BBB", 2500.00, 3),
+("CCC", "CCC", 2500.00, 4);
+
+INSERT INTO resenas
+(comentario_resena, id_usuario)
+VALUES
+("Excelente =)", 1),
+("Buenos cafes", 5);
+
+INSERT INTO configs_carusel
+(img_config_carrusel, id_empleado)
+VALUES
+("img1.jpg", 2),
+("img2.jpg", 2),
+("img3.jpg", 2),
+("img4.jpg", 2);
+
+INSERT INTO productos 
+(nombre_producto, descripcion_producto, precio_unitario_producto, stock_producto, img_producto, slug_producto,
+id_categoria, especialidad_producto, estado_producto, medida_producto, id_medida)
+VALUES
+("Cafe Americano Caliente", "Cafe muy rico", 35.00, "cafe_americano.jpg", "cafe-americano-caliente", 2, "Caliente", 1, 500, 1),
+("Chai Frappe", "Chai hecho con ingredientes naturales", 54, "chai_frappe.jpg", "chai"),
