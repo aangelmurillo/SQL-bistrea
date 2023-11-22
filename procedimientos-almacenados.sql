090ace412bd4f2c11b45a5204a2f77645c63a973
Frank <frank.atrh0905@gmail.com>
2023-11-22 14:22:22 -0600

    Se cambiaron lo de id en proc almacenados y triggers

--- procedimientos-almacenados.sql
+++ procedimientos-almacenados.sql
@@ -1,5 +1,4 @@
-/*PROCEDIMIENTOS ALMACENADOS*/
-/*BIENVENIDA USUARIOS*/
+/* BIENVENIDA USUARIOS */
 SELECT * FROM usuarios;
 SELECT * FROM roles;
 
@@ -8,21 +7,22 @@ CREATE PROCEDURE bienvenida_usuario (IN id_usuario_sesion INT)
 BEGIN
     DECLARE mensaje VARCHAR(100);
     DECLARE es_cliente BOOL;
-    /*AQUI CHECO SI ES CELINTE O NO Y LO ALMACENOE N LA VARIABLE*/
+    
+    /* AQUÍ CHECO SI ES CLIENTE O NO Y LO ALMACENO EN LA VARIABLE */
     SELECT nombre_rol = 'Cliente' INTO es_cliente
     FROM usuarios
-    JOIN roles ON usuarios.id_rol = roles.id_rol
-    WHERE usuarios.id_usuario = id_usuario_sesion;
+    JOIN roles ON usuarios.id_rol = roles.id
+    WHERE usuarios.id = id_usuario_sesion;
 
     IF es_cliente THEN
         SELECT CONCAT('Bienvenido ', usuarios.nombre_usuario, ' ', usuarios.apellido_p_usuario) INTO mensaje
         FROM usuarios
-        WHERE id_usuario = id_usuario_sesion;
+        WHERE id = id_usuario_sesion;
     ELSE
         SELECT CONCAT('Bienvenido ', roles.nombre_rol) INTO mensaje
         FROM roles
-        JOIN usuarios ON usuarios.id_rol = roles.id_rol
-        WHERE usuarios.id_usuario = id_usuario_sesion;
+        JOIN usuarios ON usuarios.id_rol = roles.id
+        WHERE usuarios.id = id_usuario_sesion;
     END IF;
 
     SELECT mensaje AS mensaje;
@@ -31,62 +31,57 @@ DELIMITER ;
 
 CALL bienvenida_usuario(4);
 
-
-
-
-/*Validacion y verifacion en stock al hacer un pedido*/
-/*Aqui agarre uno de los pedidos que ya estaban, al pasarlo a php, poner el pedido id del usuario correspondiente*/
+/* VALIDACION Y VERIFICACION EN STOCK AL HACER UN PEDIDO */
 DELIMITER //
 CREATE PROCEDURE pedido_checador_stock(IN pedido_id_ingresado INT, IN cantidad_pedido_solicitada INT)
 BEGIN
-  DECLARE stock_disponible INT;
+    DECLARE stock_disponible INT;
 
-  START TRANSACTION;
-  SELECT ingreso_stock INTO stock_disponible
-  FROM stock_productos
-  WHERE id_producto = (SELECT id_producto FROM detalles_pedido WHERE id_pedido = pedido_id_ingresado);
-
-  IF cantidad_pedido_solicitada > 0 AND stock_disponible >= cantidad_pedido_solicitada THEN
-    UPDATE stock_productos
-    SET ingreso_stock = ingreso_stock - cantidad_pedido_solicitada
+    START TRANSACTION;
+    SELECT ingreso_stock INTO stock_disponible
+    FROM stock_productos
     WHERE id_producto = (SELECT id_producto FROM detalles_pedido WHERE id_pedido = pedido_id_ingresado);
 
-    UPDATE detalles_pedido
-    SET cantidad_producto = cantidad_producto + cantidad_pedido_solicitada
-    WHERE id_pedido = pedido_id_ingresado;
-    COMMIT;
-    SELECT 'Pedido realizado con éxito.' AS mensaje;
+    IF cantidad_pedido_solicitada > 0 AND stock_disponible >= cantidad_pedido_solicitada THEN
+        UPDATE stock_productos
+        SET ingreso_stock = ingreso_stock - cantidad_pedido_solicitada
+        WHERE id_producto = (SELECT id_producto FROM detalles_pedido WHERE id_pedido = pedido_id_ingresado);
 
-  ELSE
-    ROLLBACK;
-    SELECT 'No hay suficiente stock para completar el pedido.' AS mensaje;
-  END IF;
+        UPDATE detalles_pedido
+        SET cantidad_producto = cantidad_producto + cantidad_pedido_solicitada
+        WHERE id_pedido = pedido_id_ingresado;
+        COMMIT;
+        SELECT 'Pedido realizado con éxito.' AS mensaje;
+    ELSE
+        ROLLBACK;
+        SELECT 'No hay suficiente stock para completar el pedido.' AS mensaje;
+    END IF;
 END //
 DELIMITER ;
 
-/*Ejemplos de proalmacenado (pedido_checador_stock)*/
+/* EJEMPLOS DE PROCEDIMIENTO ALMACENADO (pedido_checador_stock) */
 SELECT * FROM pedidos;
 SELECT * FROM detalles_pedido;
 SELECT * FROM productos;
 SELECT * FROM stock_productos;
-UPDATE stock_productos SET ingreso_stock=500 WHERE id_stock=1;
+UPDATE stock_productos SET ingreso_stock = 500 WHERE id = 1;
 CALL pedido_checador_stock(1, 500);
 
-/*Validacion de correo y contrasena*/
+/* VALIDACION DE CORREO Y CONTRASEÑA */
 DELIMITER //
 CREATE PROCEDURE validacion_cuenta_correo_contra(IN correo_ingresado VARCHAR(120), IN contrasena_ingresada VARCHAR(100))
 BEGIN
     DECLARE usuario_id INT;
     DECLARE mensaje VARCHAR(255);
 
-    SELECT id_usuario INTO usuario_id
+    SELECT id INTO usuario_id
     FROM usuarios
     WHERE email_usuario = correo_ingresado;
-    
+
     IF usuario_id IS NOT NULL THEN
         IF (SELECT COUNT(*)
             FROM usuarios
-            WHERE id_usuario = usuario_id AND contrasena_usuario = contrasena_ingresada) > 0 THEN
+            WHERE id = usuario_id AND contrasena_usuario = contrasena_ingresada) > 0 THEN
             SET mensaje = 'Inicio de sesión exitoso';
         ELSE
             SET mensaje = 'Contraseña no válida';
@@ -99,4 +94,4 @@ BEGIN
 END //
 DELIMITER ;
 
- CALL validacion_cuenta_correo_contra ('aangelmurv@gmail.com', '1234');
+CALL validacion_cuenta_correo_contra('aangelmurv@gmail.com', '1234');
--- triggers.sql
+++ triggers.sql
@@ -1,5 +1,4 @@
-/*TRIGGERS*/
-/*Cantidad en stock*/
+/* Cantidad en stock */
 DELIMITER //
 DROP TRIGGER IF EXISTS cantidad_stock //
 CREATE TRIGGER cantidad_stock
@@ -9,17 +8,16 @@ BEGIN
   DECLARE cantidad_pedida INT;
   SELECT cantidad_producto INTO cantidad_pedida
   FROM detalles_pedido
-  WHERE id_pedido = NEW.id_pedido;
+  WHERE id_pedido = NEW.id;
   IF NEW.estado_pedido = 'En solicitud' THEN
     UPDATE stock_productos
-    SET ingreso_stock = ingreso_stock - cantidad_pedida    /*Abajito estoy buscando mi pedido id del nuevo en la tabla de detalles*/
-    WHERE id_producto = (SELECT id_producto FROM detalles_pedido WHERE id_pedido = NEW.id_pedido);
+    SET ingreso_stock = ingreso_stock - cantidad_pedida
+    WHERE id_producto = (SELECT id_producto FROM detalles_pedido WHERE id_pedido = NEW.id);
   END IF;
 END //
 DELIMITER ;
 
-
-/*Cantidad de stock en productos*/
+/* Cantidad de stock en productos */
 DELIMITER //
 DROP TRIGGER IF EXISTS actualizar_stock_producto //
 CREATE TRIGGER actualizar_stock_producto
@@ -32,8 +30,7 @@ BEGIN
 END //
 DELIMITER ;
 
-
-/*nom_cafe segun mi id_tipo_cafe*/
+/* nom_cafe según mi id_tipo_cafe */
 DELIMITER //
 CREATE TRIGGER actualizar_nom_cafe
 BEFORE INSERT ON detalles_pedido_tipo_cafe
@@ -48,7 +45,7 @@ BEGIN
 END //
 DELIMITER ;
 
-/*Actualizacion de estado_producto, si en stock hay 0 productos*/
+/* Actualización de estado_producto, si en stock hay 0 productos */
 DELIMITER //
 CREATE TRIGGER actualizar_estado_producto
 AFTER UPDATE ON stock_productos
@@ -57,8 +54,8 @@ BEGIN
   DECLARE nueva_cantidad INT;
   SELECT ingreso_stock INTO nueva_cantidad
   FROM stock_productos
-  WHERE id_stock = NEW.id_stock;
-  
+  WHERE id_stock = NEW.id;
+
   IF nueva_cantidad > 0 THEN
     UPDATE productos
     SET estado_producto = 1
@@ -71,31 +68,30 @@ BEGIN
 END //
 DELIMITER ;
 
-/*Fecha y hora actuales en stock_productos al actualizar*/
+/* Fecha y hora actuales en stock_productos al actualizar */
 DELIMITER //
 CREATE TRIGGER actualizar_fecha_stock
 BEFORE UPDATE ON stock_productos
 FOR EACH ROW
 BEGIN
-SET NEW.fecha_ingreso_stock = NOW();
-END//
+  SET NEW.fecha_ingreso_stock = NOW();
+END //
 DELIMITER ;
 
-/*precio_unitario segun precio unitario de productos*/
+/* Precio_unitario según precio unitario de productos */
 DELIMITER //
-CREATE TRIGGER actualizar_precio_unitario 
+CREATE TRIGGER actualizar_precio_unitario
 BEFORE INSERT ON detalles_pedido
 FOR EACH ROW
 BEGIN
   DECLARE precio_por_unidad DECIMAL (6,2);
   SELECT precio_unitario_producto INTO precio_por_unidad
-  FROM productos WHERE id_producto= NEW.id_producto;
+  FROM productos WHERE id_producto = NEW.id_producto;
   SET NEW.precio_unitario = precio_por_unidad;
 END //
 DELIMITER ;
 
-/*Nombre cleinte en pedidos. Se ejecuta cunado en pedidos_clientes se establece a que 
-cliente pertenece el pedido*/
+/* Nombre cliente en pedidos. Se ejecuta cuando en pedidos_clientes se establece a qué cliente pertenece el pedido */
 DELIMITER //
 CREATE TRIGGER nombre_cliente_pedidos
 AFTER INSERT ON pedidos_clientes
@@ -112,7 +108,7 @@ BEGIN
 END //
 DELIMITER ;
 
-/*precio_unitario segun precio pe de productos_extra*/
+/* Precio_unitario según precio pe de productos_extra */
 DELIMITER //
 CREATE TRIGGER precio_unitario_prod_extra
 BEFORE INSERT ON detalles_pedido_pe
@@ -120,13 +116,13 @@ FOR EACH ROW
 BEGIN
   DECLARE precio_por_unidad DECIMAL (5,2);
   SELECT precio_unitario_pe INTO precio_por_unidad
-  FROM productos_extra WHERE id_producto_extra= NEW.id_producto_extra;
-  
+  FROM productos_extra WHERE id_producto_extra = NEW.id_producto_extra;
+
   SET NEW.precio_pe = precio_por_unidad;
 END //
 DELIMITER ;
 
-/*Se saca el subtotal_pedido de detallles_pedido*/
+/* Se saca el subtotal_pedido de detalles_pedido */
 DELIMITER //
 CREATE TRIGGER subtotal_pedido
 BEFORE INSERT ON detalles_pedido
@@ -136,21 +132,21 @@ BEGIN
 END //
 DELIMITER ;
 
-/*Se actualiza el subtotal_pedido de detalles_pedido por el prodycto extra*/
+/* Se actualiza el subtotal_pedido de detalles_pedido por el producto extra */
 DELIMITER //
 CREATE TRIGGER actualizar_subtotal_pedido_producto_extra
 AFTER UPDATE ON detalles_pedido_pe
 FOR EACH ROW
 BEGIN
     UPDATE detalles_pedido
-    SET subtotal_pedido = (SELECT precio_unitario * cantidad_producto 
-                          FROM detalles_pedido 
+    SET subtotal_pedido = (SELECT precio_unitario * cantidad_producto
+                          FROM detalles_pedido
                           WHERE id_detalle_pedido = NEW.id_detalle_pedido) + NEW.precio_pe
     WHERE id_detalle_pedido = NEW.id_detalle_pedido;
 END //
 DELIMITER ;
 
-/*Se actualiza (al insertar) el subtotal_pedido de detalles_pedido por el prodycto extra*/
+/* Se actualiza (al insertar) el subtotal_pedido de detalles_pedido por el producto extra */
 DELIMITER //
 CREATE TRIGGER insert_actualizar_subtotal_pedido_producto_extra
 AFTER INSERT ON detalles_pedido_pe
@@ -164,7 +160,7 @@ BEGIN
 END //
 DELIMITER ;
 
-/*al hacer update en id_producto_extra cambia el precio_pe*/
+/* Al hacer update en id_producto_extra cambia el precio_pe */
 DELIMITER //
 CREATE TRIGGER update_precio_unitario_prod_extra
 BEFORE UPDATE ON detalles_pedido_pe
@@ -172,13 +168,13 @@ FOR EACH ROW
 BEGIN
   DECLARE precio_por_unidad DECIMAL (5,2);
   SELECT precio_unitario_pe INTO precio_por_unidad
-  FROM productos_extra WHERE id_producto_extra= NEW.id_producto_extra;
+  FROM productos_extra WHERE id_producto_extra = NEW.id_producto_extra;
 
   SET NEW.precio_pe = precio_por_unidad;
 END //
 DELIMITER ;
 
-/*Actualizacion de hora_entrega_pedido cuando sea entregado en pedidos*/
+/* Actualización de hora_entrega_pedido cuando sea entregado en pedidos */
 DELIMITER //
 CREATE TRIGGER actualizar_hora_entrega_pedido
 BEFORE UPDATE ON pedidos
@@ -189,3 +185,4 @@ BEGIN
     END IF;
 END //
 DELIMITER ;
+
